3b292ce1d174afa6e3bbc5a140631309fa8ffb8a
robin15 <komiyama.tomoaki@gmail.com>
2019-03-10 18:12:51 +0900

    add some of combo keys.

--- keyboards/ergodash/rev2/keymaps/robin15/keymap.c
+++ keyboards/ergodash/rev2/keymaps/robin15/keymap.c
@@ -55,11 +55,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
 	[_LOWER] = LAYOUT(
-    KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NO,   KC_NO,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, 
-	  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_PGUP, KC_WBAK, KC_UP,   KC_WFWD, KC_NO,   KC_NO,   KC_F12, 
-		KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_NO,   KC_NO, 
-		KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_BSPC, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, 
-		KC_NO,   KC_1 ,   KC_2,    KC_3 ,   KC_4 ,   KC_5,    KC_NO,   KC_NO,   KC_6,    KC_7,    KC_8,    KC_9 ,   KC_0 ,   KC_NO
+    KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NO,   KC_NO,   KC_F6,         KC_F7,   KC_F8,         KC_F9,   KC_F10,  KC_F11, 
+	  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_PGUP, LALT(KC_LEFT), KC_UP,   LALT(KC_RGHT), KC_NO,   KC_NO,   KC_F12, 
+		KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_PGDN, KC_LEFT,       KC_DOWN, KC_RGHT,       KC_NO,   KC_NO,   KC_NO, 
+		KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_BSPC,       KC_NO,   KC_NO,         KC_NO,   KC_NO,   KC_NO, 
+		KC_NO,   KC_1 ,   KC_2,    KC_3 ,   KC_4 ,   KC_5,    KC_NO,   KC_NO,   KC_6,          KC_7,    KC_8,          KC_9 ,   KC_0 ,   KC_NO
   ),
 
   /* Raise
