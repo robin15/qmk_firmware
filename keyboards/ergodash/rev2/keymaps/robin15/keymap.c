1f58263b4800f5902fc328cc265715b6daa85575
robin15 <komiyama.tomoaki@gmail.com>
2019-03-10 18:06:15 +0900

    add Ctrl+Alt+Del key.

--- keyboards/ergodash/rev2/keymaps/robin15/keymap.c
+++ keyboards/ergodash/rev2/keymaps/robin15/keymap.c
@@ -26,19 +26,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
    * | Tab  |   Q  |   W  |   E  |   R  |   T  |    [ |                    |   ]  |   Y  |   U  |   I  |   O  |   P  |  \   |
    * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
-   * | Ctrl |   A  |   S  |   D  |   F  |   G  |PageDn|                    |   B  |   H  |   J  |   K  |   L  |   ;  |  "   |
+   * | Ctrl |   A  |   S  |   D  |   F  |   G  |      |                    |   B  |   H  |   J  |   K  |   L  |   ;  |  "   |
    * |------+------+------+------+------+------+---------------------------+------+------+------+------+------+------+------|
-   * | Shift|   Z  |   X  |   C  |   V  |   B  | TG(2)|      |             | Bksp |   N  |   M  |   ,  |   .  |  Up  | Shift|
+   * | Shift|   Z  |   X  |   C  |   V  |   B  | TG(2)|                    | Bksp |   N  |   M  |   ,  |   .  |  Up  | Shift|
    * |-------------+------+------+------+------+------+------+------+------+------+------+------+------+------+-------------|
-   * | Esc  |  GUI |  Alt | EISU | MO(1)| Del  | MO(3)|      ||||||||      | Space| Space| Enter|Pscree| Left | Down | Right|
+   * | Esc  |  GUI |  Alt | EISU | MO(1)| Del  | MO(3)|      ||||||||      |ct+a+d| Space| Enter|Pscree| Left | Down | Right|
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
 	[_QWERTY] = LAYOUT(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,     KC_PSCR, KC_EQL,  KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, 
-	  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,     KC_LBRC, KC_RBRC, KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_BSLS, 
-		KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,     KC_NO,   KC_B,    KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, 
-		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,     TG(2),   KC_BSPC, KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_UP,   KC_SLSH, 
-		KC_ESC,  KC_LGUI, KC_LALT, EISU,    MO(1),  KC_DEL,   MO(3),   KC_SPC,  KC_SPC, KC_ENT, KC_PSCR, KC_LEFT, KC_DOWN, KC_RGHT
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,     KC_PSCR, KC_EQL,              KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, 
+	  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,     KC_LBRC, KC_RBRC,             KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_BSLS, 
+		KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,     KC_NO,   KC_B,                KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, 
+		KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,     TG(2),   KC_BSPC,             KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_UP,   KC_SLSH, 
+		KC_ESC,  KC_LGUI, KC_LALT, EISU,    MO(1),  KC_DEL,   MO(3),   LALT(LCTL(KC_DEL)),  KC_SPC, KC_ENT, KC_PSCR, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   /* Lower
