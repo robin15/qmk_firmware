fbdc927385e7fee8bc68303853ffee85a4e2a117
robin15 <komiyama.tomoaki@gmail.com>
2019-03-24 21:45:47 +0900

    bug fix

--- keyboards/ergodash/rev2/keymaps/robin15/keymap.c
+++ keyboards/ergodash/rev2/keymaps/robin15/keymap.c
@@ -80,9 +80,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
 	[_RAISE] = LAYOUT( \
+		KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_PGDN, KC_NO,   KC_NO,   KC_NO,   KC_NO,         KC_F12,        \
     KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NO,   KC_NO,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,        KC_F11,        \
 		KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, RALT(KC_LEFT), RALT(KC_RGHT), \
-		KC_NO,   KC_1 ,   KC_2,    KC_3 ,   KC_4 ,   KC_5,    KC_NO,   KC_PGUP, KC_6,    KC_7 ,   KC_8,    KC_9,    KC_0,          KC_NO,         \
+		KC_NO,   KC_1 ,   KC_2,    KC_3 ,   KC_4 ,   KC_5,    KC_NO,   KC_PGUP, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,          KC_NO,         \
 		KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_PGDN, KC_NO,   KC_NO,   KC_NO,   KC_NO,         KC_NO          \
   ),
 
