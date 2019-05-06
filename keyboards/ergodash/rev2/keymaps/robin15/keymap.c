940cf42551bf5f64aaf5e8cf718557a5d7f7d486
robin15 <komiyama.tomoaki@gmail.com>
2019-05-06 18:36:11 +0900

    mod keymap

--- keyboards/ergodash/rev2/keymaps/robin15/keymap.c
+++ keyboards/ergodash/rev2/keymaps/robin15/keymap.c
@@ -19,10 +19,14 @@ enum custom_keycodes {
 #define EISU      (LALT(KC_GRV))
 #define CT_AL_DE  (LALT(LCTL(KC_DEL)))
 #define RAISE_SPC (LT(RAISE,KC_SPC))
+#define LOWER_SPC (LT(LOWER,KC_SPC))
 #define SFT_SPC   (SFT_T(KC_SPC))
+#define SFT_SLSH  (SFT_T(KC_SLSH))
 #define ALT_PSCR  (ALT_T(KC_PSCR))
 #define CT_TB     (LCTL(KC_TAB))
 #define SFT_CT_TB (LSFT(LCTL(KC_TAB)))
+#define PSCR_LOWER (LT(KC_LALT,LOWER))
+
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -44,8 +48,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,      KC_5,     KC_GRV,   KC_EQL,  KC_6,    KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,      KC_T,     KC_QUOT,  KC_BSLS, KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,      KC_G,     KC_LBRC,  KC_RBRC, KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_ENT, \
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,      KC_B,     ADJUST,   KC_DEL,  KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_UP,   KC_SLSH, \
-    KC_INS,  KC_LGUI, KC_LALT, EISU,    RAISE_SPC, LOWER,    CT_AL_DE, KC_ENT,  KC_BSPC, SFT_SPC, ALT_PSCR,   KC_LEFT, KC_DOWN, KC_RGHT  \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,      KC_B,     ADJUST,   KC_DEL,  KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_UP,   SFT_SLSH, \
+    KC_INS,  KC_LGUI, KC_LALT, EISU,    RAISE_SPC, LOWER,    CT_AL_DE, KC_ENT,  KC_BSPC, LOWER_SPC, PSCR_LOWER,   KC_LEFT, KC_DOWN, KC_RGHT  \
   ),
 
   /* Lower
@@ -62,11 +66,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_LOWER] = LAYOUT( \
-    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,   KC_NO, KC_NO, KC_NO,  \
-    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   SFT_CT_TB, CT_TB,   KC_NO, KC_NO, KC_NO,  \
-    KC_NO,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_NO,   KC_NO,   KC_6,    KC_7,      KC_8,    KC_9,  KC_0,  KC_NO,  \
-    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,   KC_NO, KC_NO, KC_NO,  \
-    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,   KC_NO, KC_NO, KC_NO  \
+    _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   KC_PGDN, _______,   _______,   _______,   _______,         KC_F12,        \
+    _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   SFT_CT_TB, CT_TB,   _______, _______, _______,  \
+    _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, RALT(KC_LEFT), RALT(KC_RGHT), \
+    _______,   KC_1 ,   KC_2,    KC_3 ,   KC_4 ,   KC_5,    _______,   KC_PGUP, KC_6,    KC_PGDN,    KC_PGUP,    KC_9,    KC_0,          _______,         \
+    _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   KC_PGDN, KC_PGUP, _______,   _______,   _______,         _______          \
   ),
 
   /*
@@ -83,11 +87,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_RAISE] = LAYOUT( \
-    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_PGDN, KC_NO,   KC_NO,   KC_NO,   KC_NO,         KC_F12,        \
-    KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_NO,   KC_NO,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,        KC_F11,        \
-    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, RALT(KC_LEFT), RALT(KC_RGHT), \
-    KC_NO,   KC_1 ,   KC_2,    KC_3 ,   KC_4 ,   KC_5,    KC_NO,   KC_PGUP, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,          KC_NO,         \
-    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_PGDN, KC_PGUP, KC_NO,   KC_NO,   KC_NO,         KC_NO          \
+    _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,     _______,   _______, _______, _______,  \
+    _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,   _______,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,        KC_F11,        \
+    _______,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,   _______,   KC_6,    KC_7,      KC_8,    KC_9,  KC_0,  _______,  \
+    _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______, _______, _______,   _______, _______, _______,  \
+    _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,     _______,   KC_HOME, _______, KC_END  \
   ),
 
   /* Adjust
